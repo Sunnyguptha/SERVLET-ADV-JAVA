5cd83c31dad9896aef4fea72cd54885d262b5507
sunny guptha <sunnykumargupta254@gmail.com>
2025-04-29 17:48:28 +0530

    initil file update

--- LifeCycleApp/src/main/java/com/digrii/servletjava/TestSrv.java
+++ LifeCycleApp/src/main/java/com/digrii/servletjava/TestSrv.java
@@ -1,5 +1,34 @@
 package com.digrii.servletjava;
 
-public class TestSrv {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestSrv extends HttpServlet{
+
+	public void init(ServletConfig config) throws ServletException
+	{
+		System.out.println("init-method");
+	}
+	
+	public void service(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException {
+		PrintWriter pw=res.getWriter();
+		
+		System.out.println("service method called..");
+		
+		pw.println("<center><h1>This is service method</h1></center>");
+		
+		pw.close();
+	}
+	
+	public void destroy() {
+		System.out.println("destroy method called");
+	}
+	
+	
 }
